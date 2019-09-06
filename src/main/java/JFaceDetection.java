d666929386ad14053f8c953c421d6bc7890b44b2
Joker <a.janars@gmail.com>
2019-09-07 01:54:24 +0600

    Это наверное последное

--- src/main/java/JFaceDetection.java
+++ src/main/java/JFaceDetection.java
@@ -13,10 +13,25 @@ import java.io.*;
 import java.net.URL;
 
 public class JFaceDetection {
+    int needWidth = 248, needHeight = 366;
+    static double coefficient = 0.5;
     public static void main(String[] args) throws IOException {
+
+        run("uploads/000126000086.JPG");
+        run("uploads/000616501476.JPG");
+        run("uploads/000621601320.JPG");
+        run("uploads/000703500609.jpeg");
+        run("uploads/010213550237.JPG");
+        run("uploads/010315601001.JPG");
+        run("uploads/010609600725.JPG");
+        run("uploads/010816500813.JPG");
+        run("uploads/010817000064.jpeg");
+        run("uploads/011003500658.JPG");
+        run("uploads/011031501371.JPG");
+
+    }
+    public static void run(String iin) throws IOException {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        //System.out.println("Done");
-        String iin = "uploads/001228601169.JPG";
         String imageUrl = "http://distance.atu.kz/turniket/" + iin;
 
         saveImage(imageUrl, iin);
@@ -27,23 +42,32 @@ public class JFaceDetection {
 
         MatOfRect faceDetection = new MatOfRect();
         cc.detectMultiScale(src, faceDetection);
-        System.out.println(String.format("Кол-во лиц: %d", faceDetection.toArray().length));
+        while(faceDetection.toArray().length == 0) {
+            System.out.println(String.format("Кол-во лиц: %d", faceDetection.toArray().length));
+            Mat src1 = src;
+            Core.rotate(src1, src, Core.ROTATE_90_COUNTERCLOCKWISE);
+            Imgcodecs.imwrite(iin, src);
+            cc.detectMultiScale(src, faceDetection);
+            System.out.println("Поворот изображения: " + iin);
+        }
+        int max = 0;
         for (Rect rect:faceDetection.toArray()){
-            System.out.println(rect.width + ", " + rect.height);
-            Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255), 3);
-            BufferedImage inImage = null;
-            try {
-                inImage = ImageIO.read(new File(iin));
-                BufferedImage outImage = cropImage(inImage, rect);
-                File outputfile = new File(iin);
-                ImageIO.write(outImage, "jpg", outputfile);
-            } catch (IOException e) {
+            if (max < rect.width) {
+                System.out.println(rect.width + ", " + rect.height);
+                Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255), 3);
+                BufferedImage inImage = null;
+                try {
+                    inImage = ImageIO.read(new File(iin));
+                    BufferedImage outImage = cropImage(inImage, rect);
+                    String fileName = iin.substring(iin.lastIndexOf('/') + 1);
+                    File outputfile = new File("uploads/new" + fileName);
+                    ImageIO.write(outImage, "jpg", outputfile);
+                } catch (IOException e) {
+                }
+                max = rect.width;
             }
         }
-
-        //Imgcodecs.imwrite(iin, src);
-        System.out.println("Image Detection Finished");
-
+        System.out.println("Обнаружение лиц закончено");
     }
     public static void saveImage(String imageUrl, String destinationFile) throws IOException {
         URL url = new URL(imageUrl);
@@ -61,7 +85,12 @@ public class JFaceDetection {
         os.close();
     }
     private static BufferedImage cropImage(BufferedImage src, Rect rect) {
-        BufferedImage dest = src.getSubimage(rect.x, rect.y, rect.width, rect.height);
+        int x = (rect.x < (int)(0.6 * coefficient * rect.width)) ? 0 : (rect.x - (int)(0.6 * coefficient * rect.width));
+        int y = (rect.y < (int)(0.7 * coefficient * rect.height)) ? 0 : (rect.y - (int)(0.7 * coefficient * rect.height));
+        int width = ((int)(3 * coefficient * rect.width + x) >= src.getWidth()) ? (src.getWidth() - x) : (int)(3 * coefficient * rect.width);
+        int height = ((int)(4 * coefficient * rect.height + y) >= src.getHeight()) ? (src.getHeight() - y) : (int)(4 * coefficient * rect.height);
+        //System.out.println(height);
+        BufferedImage dest = src.getSubimage(x, y, width, height);
         return dest;
     }
 }
